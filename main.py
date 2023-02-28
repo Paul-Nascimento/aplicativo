21ceddbbaeded44cb7ecbfb5112014e69162acf6
Paul-Nascimento <94070819+Paul-Nascimento@users.noreply.github.com>
2023-02-27 22:11:08 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request,url_for,redirect,flash,jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def menu():
+    return render_template("menu.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run()
